5ba53798a82200e939099dff640d5af96d253e65
Joseph Mallari <joseph@via-health.de>
2025-10-09 14:56:18 +0200

    clean up windows app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useRef, useEffect } from "react";
-import RecordingControls from "./components/RecordingControls/RecordingControls";
 import PlaybackControls from "./components/PlaybackControls/PlaybackControls";
 import WaveformVisualizer from "./components/WaveformVisualizer/WaveformVisualizer";
 import type { WaveformVisualizerRef } from "./components/WaveformVisualizer/WaveformVisualizer";
 import LiveTranscriptionDisplay from "./components/LiveTranscriptionDisplay/LiveTranscriptionDisplay";
 import TranscriptionDisplay from "./components/TranscriptionDisplay/TranscriptionDisplay";
 import { SystemAudioDetection } from "./components/SystemAudioDetection/SystemAudioDetection";
-import { SystemAudioCapture } from "./components/SystemAudioCapture/SystemAudioCapture";
 import { useAudioRecorder } from "./hooks/useAudioRecorder";
 import "./App.css";
 
@@ -20,14 +18,6 @@ function App() {
     audioElementRef,
     transcriptionState,
     liveTranscriptionState,
-
-    // recording functions
-    startRecording,
-    stopRecording,
-    pauseRecording,
-    resumeRecording,
-    resetRecording,
-    downloadRecording,
 
     // playback functions
     playRecording,
@@ -68,19 +58,8 @@ function App() {
 
       <SystemAudioDetection />
 
-      <SystemAudioCapture />
 
       <WaveformVisualizer ref={waveformRef} />
-
-      <RecordingControls
-        recordingState={recordingState}
-        onStartRecording={startRecording}
-        onStopRecording={stopRecording}
-        onPauseRecording={pauseRecording}
-        onResumeRecording={resumeRecording}
-        onResetRecording={resetRecording}
-        onDownloadRecording={downloadRecording}
-      />
 
       <LiveTranscriptionDisplay
         liveTranscriptionState={liveTranscriptionState}
